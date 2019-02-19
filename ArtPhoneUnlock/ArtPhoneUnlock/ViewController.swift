060b295e7b32f4dcfa42e51791bf2152883c424a
andieGH <amgreenh@ucalgary.ca>
2019-02-19 11:38:57 -0700

    Added art phone unlock

---
+++ ArtPhoneUnlock/ArtPhoneUnlock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtPhoneUnlock
+//
+//  Created by Andie Green-Harrison on 2019-02-19.
+//  Copyright © 2019 Andie Green-Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ArtPhoneUnlock/ArtPhoneUnlockTests/ArtPhoneUnlockTests.swift
@@ -0,0 +1,34 @@
+//
+//  ArtPhoneUnlockTests.swift
+//  ArtPhoneUnlockTests
+//
+//  Created by Andie Green-Harrison on 2019-02-19.
+//  Copyright © 2019 Andie Green-Harrison. All rights reserved.
+//
+
+import XCTest
+@testable import ArtPhoneUnlock
+
+class ArtPhoneUnlockTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
