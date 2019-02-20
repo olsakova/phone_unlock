75817c5e37eeedabec2d06925c92f20176f0f352
Michaela <mikimika@gmail.com>
2019-02-20 12:46:10 -0800

    falling notes

--- MusicPhoneUnlock/MusicPhoneUnlock/GameScene.swift
+++ MusicPhoneUnlock/MusicPhoneUnlock/GameScene.swift
@@ -8,82 +8,53 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var gameTimer:Timer!
+    var possibleNotes = ["MusicNote1", "MusicNote2", "MusicNote3", "MusicNote4", "MusicNote5", "MusicNote6", "MusicNote7", "MusicNote8", "MusicNote9", "MusicNote10" ]
+    
+    let noteCategory:UInt32 = 0x1 << 1
+    
+    let motionManger = CMMotionManager()
+    var xAcceleration:CGFloat = 0
     
     override func didMove(to view: SKView) {
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addNote), userInfo: nil, repeats:true)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    @objc func addNote(){
+        possibleNotes = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleNotes) as! [String]
+        
+        let note = SKSpriteNode(imageNamed: possibleNotes[0])
+        let randomNotePosition = GKRandomDistribution(lowestValue: -300, highestValue: 300)
+        let position = CGFloat(randomNotePosition.nextInt())
+        
+        note.position = CGPoint(x: position, y: self.frame.size.height + note.size.height)
+        
+        note.physicsBody = SKPhysicsBody(rectangleOf: note.size)
+        note.physicsBody?.isDynamic = true
+        
+        note.physicsBody?.categoryBitMask = noteCategory
+        note.physicsBody?.collisionBitMask = 0
+        
+        self.addChild(note)
+        let animationDuration:TimeInterval = 6
+        var actionArray = [SKAction]()
+        
+        //actionArray.append(SKAction.move(to: CGPoint(x: position, y: -note.size.height), duration: animationDuration))
+        actionArray.append(SKAction.move(to: CGPoint(x: position, y: -500), duration: animationDuration))
+        actionArray.append(SKAction.removeFromParent())
+        
+        note.run(SKAction.sequence(actionArray))
     }
 }
