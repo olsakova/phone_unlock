1ca2660c9f0b9cb8832d3d05f52e6517a54ce741
Michaela <mikimika@gmail.com>
2019-02-21 11:32:37 -0800

    notes clickable and transition with password

---
+++ MusicPhoneUnlock/MusicPhoneUnlock/SuccessScene.swift
@@ -0,0 +1,20 @@
+//
+//  SuccessScene.swift
+//  MusicPhoneUnlock
+//
+//  Created by Michaela on 2019-02-21.
+//  Copyright © 2019 HCI581. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class SuccessScene: SKScene {
+    override func didMove(to view: SKView) {
+        self.backgroundColor = SKColor.green
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
